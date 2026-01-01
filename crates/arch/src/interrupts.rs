9dc2959234fa079c9972e071253530b4b0c89d10
Sola <taka9819@gmail.com>
2026-01-01 20:21:21 +0900

    interrupts: コメント削除

--- crates/arch/src/interrupts.rs
+++ crates/arch/src/interrupts.rs
@@ -5,7 +5,6 @@ use spin::Once;
 static CONTROLLER: Once<&'static (dyn InterruptController + Sync)> = Once::new();
 
 pub fn init_interrupts() {
-    // 今は PIC を採用
     CONTROLLER.call_once(|| pic::pic_controller());
     controller().init();
 }
